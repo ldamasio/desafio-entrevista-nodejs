a6a78cabd7d3e1745cb253d384b1a1ae5f3fa025
Leandro Damasio <leandro.damasio@boasnovasgestao.com.br>
2023-04-17 23:49:15 -0300

    access_token

--- desafio-entrevista-nodejs/src/auth/auth.module.ts
+++ desafio-entrevista-nodejs/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
+
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 import { jwtConstants } from './constants';
 
 @Module({
@@ -14,8 +15,8 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  controllers: [AuthController],
   providers: [AuthService],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
--- desafio-entrevista-nodejs/src/auth/auth.service.ts
+++ desafio-entrevista-nodejs/src/auth/auth.service.ts
@@ -1,22 +1,23 @@
+
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService
-      ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
-    async signIn(username: string, pass: string): Promise<any> {
-        const user = await this.usersService.findOne(username);
-        if (user?.password !== pass) {
-          throw new UnauthorizedException();
-        }
-        const { password, ...result } = user;
-        // TODO: Generate a JWT and return it here
-        // instead of the user object
-        return result;
-    }   
+  async signIn(username, pass) {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
